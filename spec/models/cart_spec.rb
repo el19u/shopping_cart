d51cce115e5ce1c6be39ecf98087c35436a7c8fe
elik <akainooo@gmail.com>
2022-02-26 01:59:29 +0800

    Add cart spec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ describe "Shopping Cart" do
+   it "Item to Cart"
+   it "if Item is same, not add Item but count will add"
+   it "drop in and drop out"
+   it "Cart total"
+   it "Seasonal Discount"
+ end
+
+ describe "Advance Feature" do
+  it "Item to Session"
+  it "Session to Item"
+ end
 end
