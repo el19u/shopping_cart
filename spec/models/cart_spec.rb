18046b8b223ed2b3648408ba33e67e3287342240
elik <akainooo@gmail.com>
2022-02-26 14:30:11 +0800

    Done Cart RSpec 2/7 #1 & #2

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,16 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
- describe "Shopping Cart" do
-   it "Item to Cart"
-   it "if Item is same, not add Item but count will add"
+ context "Shopping Cart" do
+   it "Item to Cart" do
+     cart = Cart.new
+     cart.add_item 1
+     expect(cart.empty?).to be false
+   end
+
+   it "if Item is same, not add Item but count will add" do
+     cart = Cart.new
+     3.times { cart.add_item(1) }
+     5.times { cart.add_item(2) }
+     2.times { cart.add_item(3) }
+
+     expect(cart.items.length).to be 3
+     expect(cart.items.first.quantity).to be 3
+     expect(cart.items.second.quantity).to be 5
+   end
+
    it "drop in and drop out"
    it "Cart total"
    it "Seasonal Discount"
  end
 
- describe "Advance Feature" do
+ context "Advance Feature" do
   it "Item to Session"
   it "Session to Item"
  end
 end
+
