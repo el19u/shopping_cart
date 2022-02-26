d44c2e42be35444f76a478b3d65fa7ae880fbc6a
elik <akainooo@gmail.com>
2022-02-26 14:46:19 +0800

    Add #3 spec & create Product Model

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -10,16 +10,27 @@ RSpec.describe Cart, type: :model do
 
    it "if Item is same, not add Item but count will add" do
      cart = Cart.new
-     3.times { cart.add_item(1) }
-     5.times { cart.add_item(2) }
-     2.times { cart.add_item(3) }
+     3.times{ cart.add_item(1) }
+     5.times{ cart.add_item(2) }
+     2.times{ cart.add_item(3) }
 
      expect(cart.items.length).to be 3
      expect(cart.items.first.quantity).to be 3
      expect(cart.items.second.quantity).to be 5
    end
 
-   it "drop in and drop out"
+   it "drop in and drop out" do
+     cart = Cart.new
+     p1 = Product.create(title: "DrogonBall")
+     p2 = Product.create(title: "JOJO")
+
+     4.times{ cart.add_item(p1.id) }
+     2.times{ cart.add_item(p2.id) }
+
+     expect(cart.items.first.product_id).to be p1.id
+     expect(cart.items.first.product_id).to be p2.id
+     expect(cart.items.first.product_id).to be_a Product
+   end
    it "Cart total"
    it "Seasonal Discount"
  end
